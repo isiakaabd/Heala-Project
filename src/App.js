1025a949e63055944bcfe0e5e2f32146ecc207cf
Ukpe Sophia Abel <54262136+SophiannaRoxanna@users.noreply.github.com>
2021-09-21 22:26:06 +0100

    imported react in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useActions } from "components/hooks/useActions";
 import { useSelector } from "react-redux";
 import styles from "./App.module.css";
@@ -5,13 +6,16 @@ import styles from "./App.module.css";
 const App = () => {
   const { toggleText } = useSelector((state) => state.entities);
   const { toggleHelloWorld } = useActions();
+
   return (
-    <div className={styles.container}>
-      <h1>{!toggleText ? "Hello World" : "Goodbye World."}</h1>
-      <button onClick={toggleHelloWorld} className={styles.button}>
-        Toggle Text
-      </button>
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1>{!toggleText ? "Hello World" : "Goodbye World."}</h1>
+        <button onClick={toggleHelloWorld} className={styles.button}>
+          Toggle Text
+        </button>
+      </div>
+    </>
   );
 };
 
