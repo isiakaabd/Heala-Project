be99ddc14463f8770566d01e5eaa1eedb132aae9
Isiaka Abdulahi <isiakaabd@gmail.com>
2022-05-12 13:31:52 +0100

    added

--- src/components/layouts/EnhancedTableHeader.js
+++ src/components/layouts/EnhancedTableHeader.js
@@ -11,11 +11,12 @@ const useStyles = makeStyles((theme) => ({
   },
   tableHeaderCell: {
     "&.MuiTableCell-root, &.MuiTableCell-root": {
-      fontSize: "1.65rem",
+      fontSize: "calc(1.4rem, 3vw,1.65rem)",
     },
 
     "&.MuiTableCell-root": {
-      fontSize: "1.35rem",
+      // fontSize: "1.35rem",
+      fontSize: "calc(1rem, 2vw,1.35rem)",
     },
   },
 }));
